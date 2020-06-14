e54f3a3caf1ec1fc4a4d2e4234c16fe750038fd2
lrjoshi <lokrajvet@gmail.com>
2020-06-14 12:26:17 -0400

    r format table added

--- R/pairwise_genes_comparison.R
+++ R/pairwise_genes_comparison.R
@@ -1,17 +1,23 @@
 #required packages
-library (tidyverse) 
+library (tidyverse)
 library (seqinr)
 library (DECIPHER)
+library (formattable)
 
-pairwise_genes_comparison <- function () {
 
-#importing files using file.choose option
-print ("Remeber the order you upload your files. The output will have suffix 1 and 2 according the order you upload your files.")
-print ("Import first file")
-file1 <- readLines(file.choose(new=F))
+#Provide path to the filename
+filename1 <- paste("D:/Courses/NGS Tutorials/R_Practice/gff3/gene_sequence_format.txt")
+filename2 <- paste("D:/Courses/NGS Tutorials/R_Practice/gff3/gene_sequence_format.txt")
 
-print ("Import second file")
-file2 <- readLines(file.choose(new=F))
+
+
+pairwise_genes_comparison <- function (filename1,filename2) {
+
+
+file1 <- readLines(filename1)
+
+file2 <- readLines(filename2)
+
 
 #### Parsing Files and Covert to CSV ####
 #read fasta file1
@@ -22,7 +28,7 @@ file2 <- readLines(file.choose(new=F))
 
 location <- which((str_sub(file1,1,1))==">")
 
-#start an empty vector to collect name and sequence 
+#start an empty vector to collect name and sequence
 
 name=c()
 sequence =c()
@@ -36,31 +42,31 @@ for ( i in 1:length(location)){
   name1 = file1[name_line]
   name=c(name,name1)
   #extract sequence between the names
-  #the last sequence will be missed using this strategy 
-  #so, we are using if condition to extract last sequence 
+  #the last sequence will be missed using this strategy
+  #so, we are using if condition to extract last sequence
   start= location[i]+1
   end = location[i+1]-1
   if ( i < length (location)){
-    
+
     end=end
-    
+
   } else {
-    
+
     end=length(file1)
   }
-  
+
   lines = start:end
   sequence1= as.character(paste(file1[lines],collapse = ""))
   sequence =c(sequence,sequence1)
 }
 
-#now create table using name and sequence vector 
+#now create table using name and sequence vector
 
 data <- tibble(name=name,sequence=sequence)
 
 
 
-#finally export the file 
+#finally export the file
 #before that remove preexisting file
 unlink(c("dna_table1.csv"),force=TRUE)
 write.csv(data,"dna_table1.csv")
@@ -76,7 +82,7 @@ write.csv(data,"dna_table1.csv")
 
 location2 <- which((str_sub(file2,1,1))==">")
 
-#start an empty vector to collect name and sequence 
+#start an empty vector to collect name and sequence
 
 name2=c()
 sequence2 =c()
@@ -90,31 +96,31 @@ for ( i in 1:length(location2)){
   nam = file2[name_line2]
   name2=c(name2,nam)
   #extract sequence between the names
-  #the last sequence will be missed using this strategy 
-  #so, we are using if condition to extract last sequence 
+  #the last sequence will be missed using this strategy
+  #so, we are using if condition to extract last sequence
   start2= location2[i]+1
   end2 = location2[i+1]-1
   if ( i < length (location2)){
-    
+
     end2=end2
-    
+
   } else {
-    
+
     end2=length(file2)
   }
-  
+
   lines2 = start2:end2
   sequen= as.character(paste(file2[lines2],collapse = ""))
   sequence2 =c(sequence2,sequen)
 }
 
-#now create table using name and sequence vector 
+#now create table using name and sequence vector
 
 data <- tibble(name=name2,sequence=sequence2)
 
 
 
-#finally export the file 
+#finally export the file
 #before that remove preexisting file
 unlink(c("dna_table2.csv"),force=TRUE)
 write.csv(data,"dna_table2.csv")
@@ -156,20 +162,20 @@ for (z in 1:nrow(table1)) {
   gene_name1 <- string_list1[[c(z,4)]]
   start_pos1 <- as.numeric(string_list1[[c(z,11)]])
   stop_pos1 <-as.numeric( string_list1[[c(z,12)]])
-  
-  
+
+
   gene_name2 <- string_list2[[c(z,4)]]
   start_pos2 <- as.numeric(string_list2[[c(z,11)]])
   stop_pos2 <- as.numeric(string_list2[[c(z,12)]])
-  
-  
+
+
   gene_length2 =append(gene_length2,(stop_pos2-start_pos2+1))
   gene_length1 =append(gene_length1,(stop_pos1-start_pos1+1))
-  
+
   name_vector1 <- append(name_vector1,gene_name1)
   start_position1 <- append (start_position1,start_pos1)
   stop_position1 <- append (stop_position1,stop_pos1)
-  
+
   name_vector2 <- append(name_vector2,gene_name2)
   start_position2 <- append (start_position2,start_pos2)
   stop_position2 <- append (stop_position2,stop_pos2)
@@ -177,7 +183,7 @@ for (z in 1:nrow(table1)) {
 
 
 #check if names are similar
-#otherwise terminate the funtion 
+#otherwise terminate the funtion
 #if gene names are not identical the analysis will not be correct
 
 
@@ -188,16 +194,16 @@ if (validation==T) {
   print("Validation complete. Continuing analysis.")
 } else if (validation== F) {
   stop("Stopping analysis. The name, number or order of the genes in the selected files are not same. Check package docmentation for details.")
-} 
+}
 
 
 
 
 #### ROW WISE ORGANIZATION OF GENES ####
-#create pairwise files 
-#it cretes CSV files in the pairs directory 
+#create pairwise files
+#it cretes CSV files in the pairs directory
 #one file for each gene
-#rowwise organization of genes 
+#rowwise organization of genes
 for (i in 1:nrow(table1)){
   table3 <- rbind(table1[i,],table2[i,])
   table3 <- as.data.frame(table3)
@@ -208,42 +214,42 @@ for (i in 1:nrow(table1)){
 
 
 #### CONVERT TABLE INTO FASTA ####
-#this will convert table into fasta file 
-#one fasta file for a pair of gene 
-#apply tabular to fasta funtion to all the dataframes 
+#this will convert table into fasta file
+#one fasta file for a pair of gene
+#apply tabular to fasta funtion to all the dataframes
 
 
 
 for (j in 1:11){
   infilename = paste0("genes",j,".csv")
   file <- read.csv (file=infilename, header=TRUE)
-  
+
   file = as.data.frame(file)
-  #delete if any existing file 
-  
+  #delete if any existing file
+
   # unlink(c("dna_fasta.fasta"), force=TRUE)
-  
+
   #give output filename
   outfilename = paste0("genes",j,".fasta")
   sink(outfilename)
-  
+
   for (k in 1:nrow(file)){
     name = paste0(">",file[k,1])
     sequence = paste(file[k,2])
     cat(name,sep="\n")
     cat(sequence,sep="\n")
   }
-  
-  #this is sink all the console output to the file 
+
+  #this is sink all the console output to the file
   sink()
-  
+
 }
 
 
 
 
 #### CREATING ALIGNEMENT WITH DECIPHER ####
-###now creating alignment of the files 
+###now creating alignment of the files
 #get the similarity of the pairwise alignment
 #print the alignement files
 
@@ -274,14 +280,36 @@ similarity_table <- data.frame (Gene_Name=name_vector1,
                                 Gene_Length2= gene_length2,
                                 Percentage_Similarity=((1-similarity)*100))
 
+
+
+
+
 write.csv(similarity_table,"summary_table.csv")
 
+#### creating nice table ####
+df_format <- read_csv ("summary_table.csv")
+
+nice_table <- formattable (df_format,align=c("c","c","c","c","c","c","c","c","l"),
+             list(
+               Gene_Name= formatter("span",style = ~ style(color = "black",font.weight = "bold")),
+               Start_Sequence1=color_tile ("#E4F9E3","#E4F9E3"),
+               Stop_Sequence1=color_tile ("#E4F9E3","#E4F9E3"),
+               Gene_Length1=color_tile ("#E4F9E3","#E4F9E3"),
+               Start_Sequence2=color_tile ("#D1E6FA","#D1E6FA"),
+               Stop_Sequence2=color_tile ("#D1E6FA","#D1E6FA"),
+               Gene_Length2=color_tile ("#D1E6FA","#D1E6FA"),
+
+               Percentage_Similarity =color_bar ("#FA614B")
+             ))
+
+print (nice_table)
+########Final Message#################
 
 print ("Check summary_table.csv file in the current directory.")
 print ("The suffix 1 and 2 denotes the file1 and file2 respectively.")
 print ("Thank you. End of analysis.")
 
 }
+head(similarity_table)
 
-
-pairwise_genes_comparison()
+pairwise_genes_comparison(filename1,filename2)
